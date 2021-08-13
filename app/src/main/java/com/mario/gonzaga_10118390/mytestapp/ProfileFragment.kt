96d7a7b7202085b5d60b8675f4e43ebfebd16fe6
anpanman-io <mariomuharjani@gmail.com>
2021-08-13 17:25:22 +0700

    perubahan baru

---
+++ app/src/main/java/com/mario/gonzaga_10118390/mytestapp/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.mario.gonzaga_10118390.mytestapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jovanovic.stefan.mytestapp.R
+
+// 12-08-2021 - Mario Gonzaga Muharjani - 10118390
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
